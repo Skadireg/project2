fd79f572e7ef111f344ff60cb00625d881797ce9
skadireg <shestov.maksim@mail.ru>
2020-02-04 03:41:28 +0800

    set up webpack a bit more(+js +css +scss +pug)

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// JS
+
+import './js/js-test';
+import './js/js-test2';
+
+// SCSS
+import './scss/style.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,69 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
 module.exports = {
-    
+    mode: 'development',
+//ENTRY
+    entry: {
+        main: './src/app.js'
+    },
+//OUT
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+ //PLUGINS   
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+
+//DEVSERVER
+    devServer: {
+        port: 4200,
+        hot: true
+    },
+//CLASS
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                            reloadAll: true
+                        },
+                        
+                    },
+                    'css-loader'
+                ]
+            },
+            
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: true,
+                            reloadAll: true
+                        },
+                        
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+        ]
+    }
+   
 }
